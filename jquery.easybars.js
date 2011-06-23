85c32152942d0e08e35ef76951dfa0ff94028485
James Fuller <jblotus@gmail.com>
2011-06-23 16:04:13 -0400

    more work

--- jquery.easybars.js
+++ jquery.easybars.js
@@ -1,12 +1,10 @@
 /**
  * jQuery easyBars plugin
  *
- * @author James Fuller <jblotus@gmail.com
- */
-/**
- * jQuery Simple Static Progress Bars by James Fuller <james@time4learning.com>
+ * @author James Fuller <jblotus@gmail.com>
  */
 (function($) {
+
   $.fn.extend({
     easyBars: function(options) {
 
@@ -15,25 +13,43 @@
       $(self).each(function(i, e) {
         $el = $(e);
 
-        var current = parseInt($el.find('span.current').text());
-        var total   = parseInt($el.find('span.total').text());
+        var current = parseInt($el.find('.current').text());
+        var total   = parseInt($el.find('.total').text());
         var height  = $el.height();
         var width   = $el.width();
 
-        if (current && total) {
-          $el.find('.inner-bar').css({
+        if (current !== 'NaN' && total !== 'NaN') {
+
+          var $container = $('<div>').attr('class', 'easybars').css({
+            'height': height,
+            'width': width,
+            'border' : '1px solid #ccc',
+            'position' : 'relative',
+            'margin': '5px auto',
+            'text-align': 'center'
+          });
+
+          var $inner_bar  = $('<div>').attr('class', 'inner-bar').css({
             'width'  :  Math.round(width * (current / total), 0 , 2),
             'height' :  height,
             'background-color': '#FF9933',
             'position' : 'absolute'
           });
-          $el.find('.inner-text').css({
+
+          var $inner_text = $('<div>').attr('class', 'inner-text');
+
+          $inner_text.append($el.children()).css({
             'position' : 'absolute',
             'width'  :  width,
             'height' :  height
           });
+
+          $container.append($inner_bar.append($inner_text));
+          console.log($container);
+          $el.replaceWith($container);
         }
       });
+
       return self;
     }
   });
